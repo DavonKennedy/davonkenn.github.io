96e7526a99864a79e417ebcc9839054cf3232654
DavonKennedy <101531527+DavonKennedy@users.noreply.github.com>
2022-03-13 16:42:49 -0700

    Add files via upload

---
+++ treasure.js
@@ -0,0 +1,11 @@
+const magic8 = () => {
+  var randomNum = Math.floor(Math.random() * 3)
+  if(randomNum === 0) {
+    return "It is decidedly so"
+  } else if(randomNum === 1) {
+    return "Ask again tomorrow"
+  } else if(randomNum === 2) {
+    return "Looks promising"
+  } else {
+    return "Ooops, something went wrong!"
+  }
